282bb6be61091285278ed87ceae86e89a4c0a9d8
Sting Arroyo <stingarroyo@gmail.com>
2018-02-19 03:24:04 -0500

    Solicitud a la API por HTTPS.

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import config from './config';
 const { apiKey } = config;
 
 export default function getArtists(country){
-    const URL = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${apiKey}&format=json`;
+    const URL = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${apiKey}&format=json`;
     return fetch(URL)
     .then(res => res.json())
     .then(json => json.topartists.artist)
